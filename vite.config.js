1589a2b810943d62df5f3c30c9c3d5d7b3f64ad3
Antoine <antoine.favreau84@gmail.com>
2025-02-26 20:37:01 +0100

    [pwa] add pwa support

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,41 @@ import {VitePWA} from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Bimboxa",
+        short_name: "Bimboxa",
+        description: "Bimboxa",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   base: "/pwa/",
   resolve: {
     alias: {
