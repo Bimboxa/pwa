c0ee9d546320f2f028ca923e4b178b14a5e0376b
Antoine <antoine.favreau84@gmail.com>
2025-03-13 16:59:03 +0100

    [layout] fix bottom navigation

--- src/Features/layout/components/LayoutMobile.jsx
+++ src/Features/layout/components/LayoutMobile.jsx
@@ -1,14 +1,22 @@
+import {useSelector} from "react-redux";
+
 import BoxFlexV from "./BoxFlexV";
 import TopBar from "./TopBar";
 import SectionViewer from "./SectionViewer";
 import BottomBarMobile from "./BottomBarMobile";
+import MainListPanel from "Features/listPanel/components/MainListPanel";
 
 export default function LayoutMobile() {
+  // data
+
+  const viewModeInMobile = useSelector((s) => s.layout.viewModeInMobile);
+
   return (
     <BoxFlexV>
       <TopBar />
       <BoxFlexV>
-        <SectionViewer />
+        {viewModeInMobile === "MAP" && <SectionViewer />}
+        {viewModeInMobile === "LIST" && <MainListPanel />}
       </BoxFlexV>
       <BottomBarMobile />
     </BoxFlexV>
