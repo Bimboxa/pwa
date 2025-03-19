f1caeb5055a57ee2a75fe59bbd1ebbdacc738fd6
Antoine <antoine.favreau84@gmail.com>
2025-03-19 21:32:55 +0100

    [servicesCredentials] debug

--- src/Features/servicesCredentials/services.js
+++ src/Features/servicesCredentials/services.js
@@ -1,17 +1,24 @@
 const API_URL = import.meta.env.VITE_WORKER_URL_SERVICES_CREDENTIALS;
 
 export async function fetchServicesCredentials(token) {
-  const response = await fetch(API_URL, {
-    method: "GET",
-    mode: "cors",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-  });
-  const data = await response.json();
-  //
-  console.log("[fetchServicesCredentials] data:", data);
-  //
-  return data;
+  try {
+    console.log("[fetchServicesCredentials] data0:", response);
+    const response = await fetch(API_URL, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    console.log("[fetchServicesCredentials] data1:", response);
+    //
+    const data = await response.json();
+    //
+    console.log("[fetchServicesCredentials] data2:", data);
+    //
+    return data;
+  } catch (err) {
+    console.error("[fetchServicesCredentials] error:", err);
+  }
 }
