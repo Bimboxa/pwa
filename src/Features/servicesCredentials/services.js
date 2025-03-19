a4082ea0057d32c7a34891892c97c16ff46e3073
Antoine <antoine.favreau84@gmail.com>
2025-03-19 21:56:16 +0100

    [debug] change fetch header

--- src/Features/servicesCredentials/services.js
+++ src/Features/servicesCredentials/services.js
@@ -3,17 +3,23 @@ const API_URL = import.meta.env.VITE_WORKER_URL_SERVICES_CREDENTIALS;
 export async function fetchServicesCredentials(token) {
   try {
     console.log("[fetchServicesCredentials] data0:");
+
     const response = await fetch(API_URL, {
       method: "GET",
       mode: "cors",
       headers: {
         Authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
+        "X-Custom-Header": "Debug",
       },
     });
+
     console.log("[fetchServicesCredentials] data1:", response);
     //
-    const data = await response.json();
+    let data;
+    if (response.ok) {
+      data = await response.json();
+    }
     //
     console.log("[fetchServicesCredentials] data2:", data);
     //
