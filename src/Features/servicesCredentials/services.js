e2fdcd3ce3e5247285106fa7f9c563c26c836ca8
Antoine <antoine.favreau84@gmail.com>
2025-03-19 18:30:45 +0100

    [servicesCredentials] fix

--- src/Features/servicesCredentials/services.js
+++ src/Features/servicesCredentials/services.js
@@ -2,8 +2,11 @@ const API_URL = import.meta.env.VITE_WORKER_URL_SERVICES_CREDENTIALS;
 
 export async function fetchServicesCredentials(token) {
   const response = await fetch(API_URL, {
+    method: "GET",
+    mode: "cors",
     headers: {
       Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
     },
   });
   const data = await response.json();
