f0b3951ecc134a4c6c3b19d799d80d758bc7434f
Antoine <antoine.favreau84@gmail.com>
2025-03-19 16:10:16 +0100

    [auth] change style

---
+++ src/Features/auth/components/ButtonSignIn.jsx
@@ -0,0 +1,29 @@
+import {useClerk} from "@clerk/clerk-react";
+
+import {Box, Typography, Button} from "@mui/material";
+
+export default function ButtonSignIn() {
+  // strings
+
+  const signInS = "Se connecter";
+
+  // data
+
+  const {redirectToSignIn} = useClerk();
+
+  // handlers
+
+  function handleClick() {
+    try {
+      // Redirect to Clerk-hosted Sign-In page
+      redirectToSignIn();
+    } catch (err) {
+      console.error("Error during sign-in:", err);
+    }
+  }
+  return (
+    <Button onClick={handleClick} color="secondary" variant="contained">
+      {signInS}
+    </Button>
+  );
+}
