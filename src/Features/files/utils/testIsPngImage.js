a5962ecf128fc84bbe0c58175019caa143fc8cd8
Antoine <antoine.favreau84@gmail.com>
2025-04-02 14:20:46 +0200

    [fix]

--- src/Features/files/utils/testIsPngImage.js
+++ src/Features/files/utils/testIsPngImage.js
@@ -1,4 +1,9 @@
 export default function testIsPngImage(file) {
   console.log("testIsPngImage", file.type);
-  return file.type === "image/png";
+  //return file.type === "image/png";
+  return (
+    file.type === "image/png" ||
+    file.type === "image/jpeg" ||
+    file.type === "image/jpg"
+  );
 }
