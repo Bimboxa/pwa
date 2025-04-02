a7880c7f1909b04c202501f782006c53f4bd9754
Antoine <antoine.favreau84@gmail.com>
2025-04-02 13:01:38 +0200

    debug remove timeout

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,8 @@ import {LicenseInfo} from "@mui/x-license";
 const muiLicenseKey = import.meta.env.VITE_MUI_LICENSE_KEY;
 LicenseInfo.setLicenseKey(muiLicenseKey);
 
-setTimeout(() => {
-  createRoot(document.getElementById("root")).render(
-    <StrictMode>
-      <MainApp />
-    </StrictMode>
-  );
-}, 7000);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <MainApp />
+  </StrictMode>
+);
