273d862ca84e703cd9aa441ac44b25e6e3774a5b
Antoine <antoine.favreau84@gmail.com>
2025-04-06 21:09:15 +0200

    [remoteContainer] fix

--- src/Features/dropbox/services/openDropboxAuthPopup.js
+++ src/Features/dropbox/services/openDropboxAuthPopup.js
@@ -4,11 +4,6 @@ export default async function openDropboxAuthPopup(clientId) {
   const CLIENT_ID = clientId;
   const REDIRECT_URI = import.meta.env.VITE_DROPBOX_REDIRECT_URI;
 
-  console.log(
-    "[openDropboxAuthPopup] REDIRECT_URI & CLIENT_ID",
-    REDIRECT_URI,
-    CLIENT_ID
-  );
   const {codeVerifier, codeChallenge} = await generatePKCE();
 
   sessionStorage.setItem("dropbox_pkce_verifier", codeVerifier);
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import {createRoot} from "react-dom/client";
 import MainApp from "App/components/MainApp";
 
 import {LicenseInfo} from "@mui/x-license";
+import PageRemoteContainerRedirect from "App/components/PageRemoteContainerRedirect";
 const muiLicenseKey = import.meta.env.VITE_MUI_LICENSE_KEY;
 LicenseInfo.setLicenseKey(muiLicenseKey);
 
+// Check if the URL contains "/remote-container-redirect"
+const url = new URL(window.location.href);
+const isRedirectRoute = url.pathname === "/remote-container-redirect";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MainApp />
+    {isRedirectRoute ? <PageRemoteContainerRedirect /> : <MainApp />}
   </StrictMode>
 );
