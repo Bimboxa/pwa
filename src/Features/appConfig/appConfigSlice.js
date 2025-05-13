046bd75987509b9c7c7c5cf61cb3d6501e4dc882
Antoine <antoine.favreau84@gmail.com>
2025-05-13 17:07:38 +0200

    [version] change

--- src/Features/appConfig/appConfigSlice.js
+++ src/Features/appConfig/appConfigSlice.js
@@ -3,7 +3,7 @@ import {createSlice} from "@reduxjs/toolkit";
 const appConfigSlice = createSlice({
   name: "appConfig",
   initialState: {
-    appVersion: "0.9.15",
+    appVersion: "0.9.16",
     newVersionAvailable: false,
     value: null,
     openAppConfig: false,
