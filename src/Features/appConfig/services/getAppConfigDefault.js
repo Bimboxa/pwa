cf57212e68a9e0f8f96413d01a02b3754dd3d9e1
Antoine FAVREAU <FVA@MacBook-Pro-de-FVA.local>
2025-07-17 09:19:17 +0200

    [fix] fix appConfig import

--- src/Features/appConfig/services/getAppConfigDefault.js
+++ src/Features/appConfig/services/getAppConfigDefault.js
@@ -1,7 +1,7 @@
-import _appConfigRaw from "../data/_appConfig.yaml?raw";
+import appConfigRaw from "../data/appConfigDefault.yaml?raw";
 import yaml from "js-yaml";
 
 export default async function getAppConfigDefault() {
-  const appConfig = yaml.load(_appConfigRaw);
+  const appConfig = yaml.load(appConfigRaw);
   return appConfig;
 }
