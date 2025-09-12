e58a19dc1c72169bd742cb79e0588588432d8b9e
Antoine FAVREAU <FVA@MacBook-Pro-de-FVA.local>
2025-09-12 17:49:16 +0200

    [appConfig] fix error when importing config

--- src/Features/appConfig/services/getAppConfigDefault.js
+++ src/Features/appConfig/services/getAppConfigDefault.js
@@ -1,13 +1,20 @@
 import appConfigRawDefault from "../data/appConfig_default.yaml?raw";
 
-import appConfigRawLei from "../data/appConfig_lei.yaml?raw";
-import appConfigRawEdx from "../data/appConfig_edx.yaml?raw";
 import yaml from "js-yaml";
 
 export default async function getAppConfigDefault({ configCode }) {
   let appConfigRaw = appConfigRawDefault;
-  if (configCode === "lei") appConfigRaw = appConfigRawLei;
-  if (configCode === "edx") appConfigRaw = appConfigRawEdx;
+
+  if (configCode) {
+    try {
+      let url = `../data/appConfig_${configCode}.yaml?raw`;
+      const { default: appConfigRawXxx } = await import(url);
+      appConfigRaw = appConfigRawXxx;
+    } catch (error) {
+      console.warn("appConfig_xxx.yaml not found, using default config");
+      // Keep using appConfigRawDefault
+    }
+  }
 
   const appConfig = yaml.load(appConfigRaw);
   return appConfig;
