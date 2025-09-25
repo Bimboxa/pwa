0d68510b5aa57ba3b7945a484016d44021ca6d12
Antoine FAVREAU <FVA@MacBook-Pro-de-FVA.local>
2025-09-25 09:38:52 +0200

    [auth] add userProfile offline

---
+++ src/Features/auth/components/DialogUserProfile.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+
+import { setUserProfile } from "../authSlice";
+
+import { Box, Typography, TextField } from "@mui/material";
+
+import DialogGeneric from "Features/layout/components/DialogGeneric";
+import ButtonGeneric from "Features/layout/components/ButtonGeneric";
+
+export default function DialogUserProfile({ open, onClose }) {
+  const dispatch = useDispatch();
+
+  // strings
+
+  const standaloneS = "Auteur des plans";
+
+  const descriptionS = "Modifiez le nom utilisé comme auteur des plans.";
+
+  const placeholder = "ex: Paul MAGNES";
+
+  const saveS = "Modifier";
+
+  // data
+
+  const userProfile = useSelector((s) => s.auth.userProfile);
+
+  // state
+
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    if (userProfile?.userName) setName(userProfile.userName);
+  }, [userProfile?.userName]);
+
+  // handler
+
+  function handleSave() {
+    dispatch(setUserProfile({ userName: name }));
+    onClose();
+  }
+
+  return (
+    <DialogGeneric open={open} onClose={onClose} width={300}>
+      <Box sx={{ p: 2 }}>
+        <Box sx={{ bgcolor: "background.default", p: 1, borderRadius: "4px" }}>
+          <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+            {standaloneS}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {descriptionS}
+          </Typography>
+          <TextField
+            sx={{ mt: 1 }}
+            size="small"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            fullWidth
+            placeholder={placeholder}
+          />
+          <Box sx={{ p: 1, display: "flex", justifyContent: "end", width: 1 }}>
+            <ButtonGeneric
+              label={saveS}
+              onClick={handleSave}
+              variant="contained"
+              disabled={!name.length > 0}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </DialogGeneric>
+  );
+}
--- src/Features/auth/utils/getAvatarStringFromUserProfile.js
+++ src/Features/auth/utils/getAvatarStringFromUserProfile.js
@@ -2,10 +2,23 @@ export default function getAvatarStringFromUserProfile(userProfile) {
   const firstName = userProfile?.firstName;
   const lastName = userProfile?.lastName;
   const trigram = userProfile?.trigram;
+  const userName = userProfile?.userName;
 
   if (trigram) return trigram;
 
-  if (!firstName && !lastName) return "";
+  if (!firstName && !lastName) {
+    if (userName) {
+      const nameParts = userName.split(" ");
+      if (nameParts.length > 1) {
+        return (
+          nameParts[0].substring(0, 1) +
+          nameParts[nameParts.length - 1].substring(0, 1).toUpperCase()
+        );
+      }
+      return userName.substring(0, 2).toUpperCase();
+    }
+    return "";
+  }
   if (firstName && lastName) {
     return (firstName[0] + lastName[0]).toUpperCase();
   }
