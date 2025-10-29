28af64b1759c81703f003ea092511b55981cd6aa
Antoine FAVREAU <FVA@MacBook-Pro-de-FVA.local>
2025-10-29 20:41:59 +0100

    [report] fix export edx

--- src/Features/appConfig/appConfigSlice.js
+++ src/Features/appConfig/appConfigSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const appConfigSlice = createSlice({
   name: "appConfig",
   initialState: {
-    appVersion: "1.0.26",
+    appVersion: "1.1.0",
     configCode: "edx",
     newVersionAvailable: false,
     value: null,
