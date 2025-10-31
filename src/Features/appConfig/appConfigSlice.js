37b3c7744cb69edca922a4edc07c323873f6f67f
Antoine FAVREAU <FVA@MacBook-Pro-de-FVA.local>
2025-10-31 19:46:50 +0100

    [annotations] fix sort

--- src/Features/appConfig/appConfigSlice.js
+++ src/Features/appConfig/appConfigSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const appConfigSlice = createSlice({
   name: "appConfig",
   initialState: {
-    appVersion: "1.1.3",
+    appVersion: "1.1.4",
     configCode: "edx",
     newVersionAvailable: false,
     value: null,
