c8db754a8d86bc01fa5851cc0c2c60a90ffd55a5
Antoine FAVREAU <FVA@MacBook-Pro-de-FVA.local>
2025-10-31 19:49:09 +0100

    [annotations] fix deleting

--- src/Features/annotations/hooks/useDeleteAnnotation.js
+++ src/Features/annotations/hooks/useDeleteAnnotation.js
@@ -8,7 +8,23 @@ export default function useDeleteAnnotation() {
   const dispatch = useDispatch();
 
   return async (annotationId) => {
-    if (annotationId) await db.annotations.delete(annotationId);
+    if (annotationId) {
+      const annotation = await db.annotations.get(annotationId);
+      if (annotation?.listingId) {
+        const _listing = await db.listings.get(annotation.listingId);
+        if (_listing?.sortedAnnotationIds) {
+          const updates = {
+            sortedAnnotationIds: _listing.sortedAnnotationIds.filter(
+              (id) => id !== annotationId
+            ),
+          };
+          await db.listings.update(annotation.listingId, updates);
+        }
+      } else {
+        console.warn("debug_3110_annotation_no_listing", annotation);
+      }
+      await db.annotations.delete(annotationId);
+    }
     dispatch(triggerAnnotationsUpdate());
   };
 }
