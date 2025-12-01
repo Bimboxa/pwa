aa403c3fb9296b825c858ce03d49fca0cacd7293
Antoine FAVREAU <FVA@MacBook-Pro-de-FVA.local>
2025-12-01 16:54:15 +0100

    [appVersion] 1.16

--- src/Features/appConfig/appConfigSlice.js
+++ src/Features/appConfig/appConfigSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const appConfigSlice = createSlice({
   name: "appConfig",
   initialState: {
-    appVersion: "1.1.15",
+    appVersion: "1.1.16",
     configCode: "edx",
     newVersionAvailable: false,
     value: null,
