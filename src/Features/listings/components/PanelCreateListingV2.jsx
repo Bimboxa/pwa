2dd2bc59039252042fdbe7449c6f9bec12afc324
Antoine FAVREAU <FVA@MacBook-Pro-de-FVA.local>
2026-01-07 18:07:09 +0100

    hot fix

---
+++ src/Features/listings/components/PanelCreateListingV2.jsx
@@ -0,0 +1,15 @@
+import Panel from "Features/layout/components/Panel"
+import usePresetScopes from "Features/scopeCreator/hooks/usePresetScopes";
+import resolveAppConfig from "Features/appConfig/utils/resolveAppConfig";
+
+export default function PanelCreateListingV2() {
+
+
+
+
+    return (
+        <Panel>
+
+        </Panel>
+    )
+}
