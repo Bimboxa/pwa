6301c733213b332a51eb42945d75cd9fce1fa118
Antoine FAVREAU <FVA@MacBook-Pro-de-FVA.local>
2026-01-23 11:35:20 +0100

    [useSelectedPresetScope] fix

--- src/Features/scopeCreator/hooks/useSelectedPresetScope.js
+++ src/Features/scopeCreator/hooks/useSelectedPresetScope.js
@@ -3,10 +3,12 @@ import { useSelector } from "react-redux";
 import usePresetScopes from "./usePresetScopes";
 
 export default function useSelectedPresetScope() {
-  const presetScopes = usePresetScopes();
+  const presetScopes = usePresetScopes()
   const selectedPresetScopeKey = useSelector(
     (s) => s.scopeCreator.selectedPresetScopeKey
   );
+
+  if (!presetScopes?.length) return null;
 
   return presetScopes.find((s) => s.key === selectedPresetScopeKey);
 }
