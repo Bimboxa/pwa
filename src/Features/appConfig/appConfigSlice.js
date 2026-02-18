7497c4162cf2fc1799a1c0192cc38986ed51028a
Antoine FAVREAU <FVA@MacBook-Pro-de-FVA.local>
2026-02-18 12:13:07 +0100

    [1.3.29] log config loader

--- src/Features/appConfig/appConfigSlice.js
+++ src/Features/appConfig/appConfigSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const appConfigSlice = createSlice({
   name: "appConfig",
   initialState: {
-    appVersion: "1.3.28",
+    appVersion: "1.3.29",
     configCode: null, // updated by useInitAppConfig
     newVersionAvailable: false,
     value: null,
