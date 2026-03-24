1ab038a356ed8a98b9feecf67ce7c6e795d1e9c0
Antoine Favreau <antoine.favreau84@gmail.com>
2026-03-24 07:02:58 +0100

    remove docker toggle for MAP viewer

--- src/Features/layout/components/TopBarBreadcrumbs.jsx
+++ src/Features/layout/components/TopBarBreadcrumbs.jsx
@@ -25,6 +25,7 @@ export default function TopBarBreadcrumbs() {
   const projectId = useSelector((s) => s.projects.selectedProjectId);
   const scopeId = useSelector((s) => s.scopes.selectedScopeId);
   const leftPanelDocked = useSelector((s) => s.leftPanel.leftPanelDocked);
+  const selectedViewerKey = useSelector((s) => s.viewers.selectedViewerKey);
 
   const { value: selectedProject } = useSelectedProject();
   const { value: selectedScope } = useSelectedScope();
@@ -109,7 +110,7 @@ export default function TopBarBreadcrumbs() {
     return (
       <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
         <Home />
-        <ToggleDock />
+        {selectedViewerKey !== "MAP" && <ToggleDock />}
         <Separator />
         <ButtonDialogOnboardingSelectProject />
       </Box>
@@ -128,7 +129,7 @@ export default function TopBarBreadcrumbs() {
     return (
       <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
         <Home />
-        <ToggleDock />
+        {selectedViewerKey !== "MAP" && <ToggleDock />}
         <Separator />
         <Project />
         {/* <Separator />
